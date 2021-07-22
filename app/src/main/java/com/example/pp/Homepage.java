f431b6791fe871005f1005c4c79a9f647e18cfbb
Thurunu11 <thurunu.universe@gmail.com>
2021-07-23 00:11:03 +0530

    home

--- app/src/main/java/com/example/pp/Homepage.java
+++ app/src/main/java/com/example/pp/Homepage.java
@@ -1,6 +1,7 @@
 package com.example.pp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,11 +14,39 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Homepage extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
+    CardView cardAddPet;
+    CardView cardMyPrf;
+    CardView cardMyPet;
+    CardView cardScnID;
+    CardView cardCalDoc;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         mAuth=FirebaseAuth.getInstance();
+
+        cardAddPet=findViewById(R.id.addPet);
+        cardMyPet=findViewById(R.id.card_myPets);
+        cardMyPrf=findViewById(R.id.myPrf);
+        cardScnID=findViewById(R.id.card_scnID);
+        cardCalDoc=findViewById(R.id.card_callDoc);
+
+
+        cardAddPet.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Add_pet.class));
+            }
+        });
+
+        cardMyPet.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), View_pet.class));
+            }
+        });
 
     }
 
